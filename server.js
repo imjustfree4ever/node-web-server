16bd429392d13d1677ea4bd5be84b34d16ef3ea9
unknown <imjustfree4ever@gmail.com>
2018-01-29 20:20:32 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/');
+
+app.set('view engine', 'hbs');
+
+
+
+app.use( (req, res, next) => {
+
+    var now = new Date().toString()
+    var log = `${now} :  ${req.method}  ${req.url}`;
+    fs.appendFile('server.log',`${log}\n`, (err) =>{
+        if(err)
+        console.log('Unable to append to server log');
+    });
+    console.log(log);
+    next();
+})
+
+
+
+
+// app.use( (req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+var option = {
+    dotfiles: 'ignore',
+    etag : false,
+    extensions: ['htm', 'html'],
+    index:false,
+    maxAge: 'id',
+    redirect: false,
+};
+app.use(express.static (__dirname +'/public', option));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+
+hbs.registerHelper('screamIt', (msg)=>{
+   return  msg.toUpperCase();
+})
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMsg: 'Hello world new root Page',
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle : 'About Page',
+    });
+});
+
+
+app.get('/bad', (req, res) => {
+    //res.render('help')
+});
+
+
+app.listen(3000, () =>{
+    console.log('server is up on port 3000');
+});
